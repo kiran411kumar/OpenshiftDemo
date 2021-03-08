1c68572e76d69c5f5571656300d8f1713ed3700b
kiran411kumar <kiran.nvkk@gmail.com>
2021-03-08 11:16:18 +0530

    EAP Code

---
+++ JbossEAP/src/main/java/com/nemana/OpenshiftSampleApplication.java
@@ -0,0 +1,22 @@
+package com.nemana;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ComponentScans;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@SpringBootApplication
+@ComponentScan("com.nemana.*")
+@EnableJpaRepositories
+public class OpenshiftSampleApplication  extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(OpenshiftSampleApplication.class);
+	}
+	public static void main(String[] args) {
+		SpringApplication.run(OpenshiftSampleApplication.class, args);
+	}
+
+}

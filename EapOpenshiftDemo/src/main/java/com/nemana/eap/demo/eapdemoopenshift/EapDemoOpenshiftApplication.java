9c0ad7b3eec4a43b7e4c9115175f9850fc6d18bf
kiran411kumar <52400056+kiran411kumar@users.noreply.github.com>
2021-03-22 19:26:07 +0530

    Add files via upload

---
+++ EapOpenshiftDemo/src/main/java/com/nemana/eap/demo/eapdemoopenshift/EapDemoOpenshiftApplication.java
@@ -0,0 +1,13 @@
+package com.nemana.eap.demo.eapdemoopenshift;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EapDemoOpenshiftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EapDemoOpenshiftApplication.class, args);
+	}
+
+}
---
+++ EapOpenshiftDemo/src/test/java/com/nemana/eap/demo/eapdemoopenshift/EapDemoOpenshiftApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nemana.eap.demo.eapdemoopenshift;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EapDemoOpenshiftApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
